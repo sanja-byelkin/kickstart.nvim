7ec8819b6eeec951666308796cb0180c971a6c8a
Oleksandr Byelkin <sanja@mariadb.com>
2024-12-06 19:30:01 +0100

    we use .inl C++ files

--- init.lua
+++ init.lua
@@ -696,7 +696,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {
-          filetypes = { 'c', 'cc', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+          filetypes = { 'c', 'cc', 'inl', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
         },
         cmake = {},
         -- gopls = {},
